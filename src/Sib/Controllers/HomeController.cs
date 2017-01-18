6c4663530436cc31735c488664175fab71b201b3
João Trigo Soares <joao.miguel.soares@gmail.com>
2017-01-18 22:54:59 +0000

    first building blocks of the site

--- src/Sib/Controllers/HomeController.cs
+++ src/Sib/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace Sib.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
