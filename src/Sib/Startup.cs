233c0394f44e1a289e659d7c0a4878d18b4d208f
João Trigo Soares <joao.miguel.soares@gmail.com>
2017-02-09 22:53:31 +0000

    project is working again with mongodb identity and correct project.json

--- src/Sib/Startup.cs
+++ src/Sib/Startup.cs
@@ -1,21 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Sib.Data;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity.MongoDB;
+using MongoDB.Driver;
+using Sib.Core.Settings;
 using Sib.Models;
 using Sib.Services;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Sib
 {
     public class Startup
     {
+        private readonly IHostingEnvironment _env;
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -31,6 +38,7 @@ namespace Sib
 
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
+            _env = env;
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -38,17 +46,58 @@ namespace Sib
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            // services.AddDbContext<ApplicationDbContext>(options =>
-            //     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            // services.AddIdentity<ApplicationUser, IdentityRole>()
-            //     .AddEntityFrameworkStores<ApplicationDbContext>()
-            //     .AddDefaultTokenProviders();
+            services.Configure<MongoDbSettings>(Configuration.GetSection("MongoDbSettings"));
+            services.AddSingleton<IUserStore<ApplicationUser>>(provider =>
+            {
+                var options = provider.GetService<IOptions<MongoDbSettings>>();
+                var client = new MongoClient(options.Value.ConnectionString);
+                var database = client.GetDatabase(options.Value.DatabaseName);
+                var collection = database.GetCollection<ApplicationUser>(nameof(ApplicationUser));
 
-            
+                return new UserStore<ApplicationUser>(collection);
+            });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                var dataProtectionPath = Path.Combine(_env.WebRootPath, "identity-artifacts");
+                options.Cookies.ApplicationCookie.AuthenticationScheme = "ApplicationCookie";
+                options.Cookies.ApplicationCookie.DataProtectionProvider =
+                    DataProtectionProvider.Create(dataProtectionPath);
+                options.Lockout.AllowedForNewUsers = true;
+            });
+
+            // Services used by identity
+            services.AddAuthentication(options =>
+            {
+                // This is the Default value for ExternalCookieAuthenticationScheme
+                options.SignInScheme = new IdentityCookieOptions().ExternalCookieAuthenticationScheme;
+            });
+
+            // Hosting doesn't add IHttpContextAccessor by default
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddOptions();
+            services.AddDataProtection();
+
+            services.TryAddSingleton<IdentityMarkerService>();
+            services.TryAddSingleton<IUserValidator<ApplicationUser>, UserValidator<ApplicationUser>>();
+            services.TryAddSingleton<IPasswordValidator<ApplicationUser>, PasswordValidator<ApplicationUser>>();
+            services.TryAddSingleton<IPasswordHasher<ApplicationUser>, PasswordHasher<ApplicationUser>>();
+            services.TryAddSingleton<ILookupNormalizer, UpperInvariantLookupNormalizer>();
+            services.TryAddSingleton<IdentityErrorDescriber>();
+            services.TryAddSingleton<ISecurityStampValidator, SecurityStampValidator<ApplicationUser>>();
+            services
+                .TryAddSingleton
+                <IUserClaimsPrincipalFactory<ApplicationUser>, UserClaimsPrincipalFactory<ApplicationUser>>();
+            services.TryAddSingleton<UserManager<ApplicationUser>, UserManager<ApplicationUser>>();
+            services.TryAddScoped<SignInManager<ApplicationUser>, SignInManager<ApplicationUser>>();
+
+            AddDefaultTokenProviders(services);
 
             services.AddMvc();
+
+
 
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
@@ -64,7 +113,6 @@ namespace Sib
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
                 app.UseBrowserLink();
             }
             else
@@ -74,7 +122,18 @@ namespace Sib
 
             app.UseStaticFiles();
 
-            app.UseIdentity();
+            app.UseIdentity()
+                .UseFacebookAuthentication(new FacebookOptions
+                {
+                    AppId = "",
+                    AppSecret = "",
+                    AutomaticAuthenticate = true,
+                    AutomaticChallenge = true,
+                    Events = new OAuthEvents
+                    {
+                        
+                    }
+                });
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
 
@@ -84,6 +143,28 @@ namespace Sib
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+
+        private static void AddDefaultTokenProviders(IServiceCollection services)
+        {
+            var dataProtectionProviderType =
+                typeof(DataProtectorTokenProvider<>).MakeGenericType(typeof(ApplicationUser));
+            var phoneNumberProviderType = typeof(PhoneNumberTokenProvider<>).MakeGenericType(typeof(ApplicationUser));
+            var emailTokenProviderType = typeof(EmailTokenProvider<>).MakeGenericType(typeof(ApplicationUser));
+            AddTokenProvider(services, TokenOptions.DefaultProvider, dataProtectionProviderType);
+            AddTokenProvider(services, TokenOptions.DefaultEmailProvider, emailTokenProviderType);
+            AddTokenProvider(services, TokenOptions.DefaultPhoneProvider, phoneNumberProviderType);
+        }
+
+        private static void AddTokenProvider(IServiceCollection services, string providerName, Type provider)
+        {
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Tokens.ProviderMap[providerName] = new TokenProviderDescriptor(provider);
+            });
+
+            services.AddSingleton(provider);
         }
     }
 }
