cf7603702e7a865c2d4104a4506bb3b5058d2241
João Trigo Soares <joao.miguel.soares@gmail.com>
2017-02-09 15:46:08 +0000

    mongodb identity is working, just need to adjust project.json again

---
+++ src/Sib.Core/Domain/SibUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sib.Core.Domain
+{
+    public class SibUser
+    {
+    }
+}
---
+++ src/Sib.Core/Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Sib.Core.Settings
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
