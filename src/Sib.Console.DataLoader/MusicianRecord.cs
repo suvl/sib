a2ba35c77f56664e67465491128966d5999686e4
João Trigo Soares <joao@jtsoar.es>
2017-08-13 15:52:39 +0100

    migration to dotnet core 2.0 + data loading to azure

---
+++ src/Sib.Console.DataLoader/MusicianRecord.cs
@@ -0,0 +1,27 @@
+﻿namespace Sib.Console.DataLoader
+{
+    public class MusicianRecord
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Birthdate { get; set; }
+        public string Cellphone { get; set; }
+        public string CitizenCard { get; set; }
+        public string TaxId { get; set; }
+        public string HealthId { get; set; }
+        public string FatherName { get; set; }
+        public string FatherContact { get; set; }
+        public string MotherName { get; set; }
+        public string MotherContact { get; set; }
+        public string ParentsEmail { get; set; }
+        public string JoinDate { get; set; }
+        public string Section { get; set; }
+        public string Image { get; set; }
+        public string Created { get; set; }
+        public string Modified { get; set; }
+        public string NetworkId { get; set; }
+    }
+}
---
+++ src/Sib.Console.DataLoader/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Sib.Console.DataLoader
+{
+    using MongoDB.Driver;
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        const string FileName = @"musicians.json";
+
+        static void Main(string[] args)
+        {
+            var content = System.IO.File.ReadAllText(FileName);
+            var musicianList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<MusicianRecord>>(content);
+            musicianList.ForEach(m => Console.WriteLine(m.Name));
+
+            MongoClient client = new MongoClient(new MongoUrl("")); // get this from config
+            IMongoDatabase database = client.GetDatabase("Sib_Dev");
+            IMongoCollection<MusicianRecord> collection = database.GetCollection<MusicianRecord>(nameof(MusicianRecord));
+
+            collection.InsertMany(musicianList);
+        }
+    }
+}
